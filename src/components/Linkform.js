2234fd265e363e858f6efbbcac91d2601883c25f
Saugat <saugatpokhrel77@gmail.com>
2023-07-22 16:28:13 +0545

    readme updated

--- src/components/Linkform.js
+++ src/components/Linkform.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Loader from './Loader';
 
 const Linkform = () => {
@@ -7,18 +7,34 @@ const Linkform = () => {
     return urlregex.test(url);
   };
 
-  const [load, setLoad] = useState(false); // Use state to track the loading status
+  const [load, setLoad] = useState(false);
+  const [shortenedURL, setShortenedURL] = useState('');
+  const [online, setOnline] = useState(navigator.onLine);
+
+  useEffect(() => {
+    const handleOnlineStatusChange = () => {
+      setOnline(navigator.onLine);
+    };
+
+    window.addEventListener('online', handleOnlineStatusChange);
+    window.addEventListener('offline', handleOnlineStatusChange);
+
+    return () => {
+      window.removeEventListener('online', handleOnlineStatusChange);
+      window.removeEventListener('offline', handleOnlineStatusChange);
+    };
+  }, []);
 
   const handleShortenClick = () => {
     setLoad(true); // Show the loader
+    const messageElement = document.querySelector('#message');
+    const copyElement = document.querySelector('#cpye');
+    messageElement.style.display = 'none';
 
     const inputElement = document.querySelector('.form-control');
     const inputValue = inputElement.value;
-    // console.log('Input URL:', inputValue);
 
     if (validateURL(inputValue)) {
-      // console.log('Valid URL');
-
       fetch('http://localhost:5000/api/url/shorten', {
         method: 'POST',
         headers: {
@@ -26,40 +42,61 @@ const Linkform = () => {
         },
         body: JSON.stringify({ longUrl: inputValue }),
       })
-      .then((res) => res.json())
-      .then((data) => {
-        // console.log('Data:', data);
-
+        .then((res) => res.json())
+        .then((data) => {
           inputElement.value = '';
-          // console.log('Input URL:', inputValue);
-
-          const messageElement = document.querySelector('#message');
           messageElement.classList.add('alert-success');
           messageElement.classList.remove('alert-danger');
           setLoad(false); // Hide the loader
-
-          messageElement.innerHTML = `Shortened URL created: <a href="http://localhost:3000/${data}">Your-link</a>`;
-     
+          messageElement.style.display = 'inline';
+          setShortenedURL(data);
+          messageElement.innerHTML = `
+            Shortened URL created: 
+            <a href="http://localhost:3000/${data}" target="_blank" rel="noopener noreferrer">Your-link</a>
+        
+          `;
+          copyElement.style.display = 'inline';
         })
         .catch((error) => {
           console.error('Error:', error);
-          setLoad(false); // Hide the loader in case of an error
+          setLoad(false);
         });
     } else {
-      // console.log('Invalid URL');
       const messageElement = document.querySelector('#message');
+      messageElement.style.display = 'none';
       messageElement.classList.add('alert-danger');
       messageElement.classList.remove('alert-success');
-      setLoad(false); // Hide the loader
+      setLoad(false);
+      messageElement.style.display = 'block';
       messageElement.innerHTML = 'Invalid URL';
-     
-
+      setTimeout(() => {
+        messageElement.style.display = 'none';
+      }, 1000);
     }
   };
 
   const handleKeyDown = (e) => {
     if (e.keyCode === 13) {
       handleShortenClick();
+    }
+  };
+
+  const copyToClipboard = (data) => {
+    if (shortenedURL) {
+      const tempInput = document.createElement('input');
+      tempInput.value = `http://localhost:3000/${shortenedURL}`;
+      document.body.appendChild(tempInput);
+      tempInput.select();
+      document.execCommand('copy');
+      document.body.removeChild(tempInput);
+      const copyElement = document.querySelector('#cpye');
+      copyElement.style.display = 'none';
+      const messageElement = document.querySelector('#message');
+      messageElement.style.display = 'block';
+      messageElement.innerHTML = 'Link copied to clipboard';
+      setTimeout(() => {
+        messageElement.style.display = 'none';
+      }, 1000);
     }
   };
 
@@ -87,10 +124,24 @@ const Linkform = () => {
           </div>
         </div>
         <div>
-        {load ? <Loader /> : ''}
+          {load ? <Loader /> : ''}
         </div>
+        {!online && (
+          <div className="alert alert-warning">
+            You are currently offline. Please check your internet connection.
+          </div>
+        )}
         <div className="alert" id="message">
-        
+        </div>
+        <div className="cpyhandle" id='cpye' style={{ display: 'none' }}>
+          <button
+            className="btn btn-outline-secondary mx-3"
+            type="button"
+            id="button-addon2"
+            onClick={() => copyToClipboard()}
+          >
+            Copy
+          </button>
         </div>
       </div>
     </div>
